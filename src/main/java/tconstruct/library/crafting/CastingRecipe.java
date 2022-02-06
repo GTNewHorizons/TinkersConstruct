a74f2f811a3e95cef9865c83d8c4789102322e8c
miozune <miozune@gmail.com>
2022-02-06 16:46:44 +0900

    Add a way to ignore NBT in casting recipes

--- src/main/java/tconstruct/library/crafting/CastingRecipe.java
+++ src/main/java/tconstruct/library/crafting/CastingRecipe.java
@@ -12,8 +12,9 @@ public class CastingRecipe
     public boolean consumeCast;
     public int coolTime;
     public FluidRenderProperties fluidRenderProperties;
+    public boolean ignoreNBT;
 
-    public CastingRecipe(ItemStack replacement, FluidStack metal, ItemStack cast, boolean consume, int delay, FluidRenderProperties props)
+    public CastingRecipe(ItemStack replacement, FluidStack metal, ItemStack cast, boolean consume, int delay, FluidRenderProperties props, boolean ignoreNBT)
     {
         castingMetal = metal;
         this.cast = cast;
@@ -21,16 +22,26 @@ public class CastingRecipe
         consumeCast = consume;
         coolTime = delay;
         fluidRenderProperties = props;
+        this.ignoreNBT = ignoreNBT;
+    }
+
+    public CastingRecipe(ItemStack replacement, FluidStack metal, ItemStack cast, boolean consume, int delay, FluidRenderProperties props)
+    {
+        this(replacement, metal, cast, consume, delay, props, false);
     }
 
     public boolean matches (FluidStack metal, ItemStack inputCast)
     {
-        if (castingMetal.isFluidEqual(metal) && ((cast != null && cast.getItemDamage() == Short.MAX_VALUE && inputCast.getItem() == cast.getItem()) || ItemStack.areItemStacksEqual(this.cast, inputCast)))
-        {
-            return true;
+        if (castingMetal.isFluidEqual(metal)) {
+            if (cast != null && cast.getItemDamage() == Short.MAX_VALUE && inputCast.getItem() == cast.getItem()) {
+                return true;
+            } else if (!ignoreNBT && ItemStack.areItemStacksEqual(this.cast, inputCast)) {
+                return true;
+            } else if (ignoreNBT && cast != null && this.cast.isItemEqual(inputCast)) {
+                return true;
+            }
         }
-        else
-            return false;
+        return false;
     }
 
     public ItemStack getResult ()
