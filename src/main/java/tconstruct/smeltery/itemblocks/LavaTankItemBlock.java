8f492aca7869c4f023cda47d752ee3d0bc92bb55
MuXiu1997 <muxiu1997@gmail.com>
2022-04-04 16:55:56 +0800

    Implement IFluidContainerItem for LavaTankItemBlock

--- src/main/java/tconstruct/smeltery/itemblocks/LavaTankItemBlock.java
+++ src/main/java/tconstruct/smeltery/itemblocks/LavaTankItemBlock.java
@@ -1,14 +1,18 @@
 package tconstruct.smeltery.itemblocks;
 
-import java.util.List;
 import mantle.blocks.abstracts.MultiItemBlock;
 import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.StatCollector;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.IFluidContainerItem;
+import tconstruct.smeltery.logic.LavaTankLogic;
 
-public class LavaTankItemBlock extends MultiItemBlock
+import java.util.List;
+
+public class LavaTankItemBlock extends MultiItemBlock implements IFluidContainerItem
 {
     public static final String blockTypes[] = { "Tank", "Gague", "Window" };
 
@@ -36,5 +40,99 @@ public class LavaTankItemBlock extends MultiItemBlock
             list.add(StatCollector.translateToLocal("searedtank3.tooltip"));
             list.add(StatCollector.translateToLocal("searedtank2.tooltip"));
         }
+    }
+
+    /**
+     * @param container
+     *         ItemStack which is the fluid container.
+     * @return FluidStack representing the fluid in the container, null if the container is empty.
+     */
+    @Override
+    public FluidStack getFluid(ItemStack container) {
+        if (container.hasTagCompound()) {
+            NBTTagCompound nbt = container.getTagCompound();
+            if (nbt.hasKey("Fluid")) {
+                return FluidStack.loadFluidStackFromNBT(nbt.getCompoundTag("Fluid"));
+            }
+        }
+        return null;
+    }
+
+    /**
+     * @param container
+     *         ItemStack which is the fluid container.
+     * @param resource
+     *         FluidStack attempting to set to the container.
+     */
+    public void setFluid(ItemStack container, FluidStack resource) {
+        boolean removeFluid = resource == null || resource.amount <= 0;
+        NBTTagCompound nbt = container.getTagCompound();
+        if (removeFluid && nbt == null) return;
+        if (nbt == null) nbt = new NBTTagCompound();
+        if (removeFluid) {
+            nbt.removeTag("Fluid");
+        } else {
+            nbt.setTag("Fluid", resource.writeToNBT(new NBTTagCompound()));
+        }
+        container.setTagCompound(nbt);
+    }
+
+
+    /**
+     * @param container
+     *         ItemStack which is the fluid container.
+     * @return Capacity of this fluid container.
+     */
+    @Override
+    public int getCapacity(ItemStack container) {
+        return LavaTankLogic.tankCapacity;
+    }
+
+    /**
+     * @param container
+     *         ItemStack which is the fluid container.
+     * @param resource
+     *         FluidStack attempting to fill the container.
+     * @param doFill
+     *         If false, the fill will only be simulated.
+     * @return Amount of fluid that was (or would have been, if simulated) filled into the
+     * container.
+     */
+    @Override
+    public int fill(ItemStack container, FluidStack resource, boolean doFill) {
+        if (container.stackSize != 1) return 0;
+        if (resource == null || resource.amount <= 0) return 0;
+        FluidStack fluidStack = this.getFluid(container);
+        if (fluidStack == null) fluidStack = new FluidStack(resource, 0);
+        if (!fluidStack.isFluidEqual(resource)) return 0;
+        int amount = Math.min(this.getCapacity(container) - fluidStack.amount, resource.amount);
+        if (doFill && amount > 0) {
+            fluidStack.amount += amount;
+            this.setFluid(container, fluidStack);
+        }
+        return amount;
+    }
+
+    /**
+     * @param container
+     *         ItemStack which is the fluid container.
+     * @param maxDrain
+     *         Maximum amount of fluid to be removed from the container.
+     * @param doDrain
+     * @return Amount of fluid that was (or would have been, if simulated) drained from the
+     * container.
+     */
+    @Override
+    public FluidStack drain(ItemStack container, int maxDrain, boolean doDrain) {
+        if (container.stackSize != 1) return null;
+        FluidStack fluidStack = this.getFluid(container);
+        if (fluidStack == null || fluidStack.amount <= 0) return null;
+        int drain = Math.min(fluidStack.amount, maxDrain);
+        if (drain <= 0) return null;
+        if (doDrain) {
+            fluidStack.amount -= drain;
+            this.setFluid(container, fluidStack);
+        }
+        return new FluidStack(fluidStack, drain);
     }
 }
