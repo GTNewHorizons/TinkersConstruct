3295295585ca120bf6a4a1613d903a0c83094a85
NexusNull <p.wellershaus@googlemail.com>
2023-01-16 14:35:39 +0100

    only break one block on shift

--- src/main/java/tconstruct/library/tools/AOEHarvestTool.java
+++ src/main/java/tconstruct/library/tools/AOEHarvestTool.java
@@ -34,6 +34,9 @@ public abstract class AOEHarvestTool extends HarvestTool {
         int sideHit = mop.sideHit;
         // int sideHit = Minecraft.getMinecraft().objectMouseOver.sideHit;
 
+        if(player.isSneaking())
+            return super.onBlockStartBreak(stack, x, y, z, player);
+
         // we successfully destroyed a block. time to do AOE!
         int xRange = breakRadius;
         int yRange = breakRadius;
