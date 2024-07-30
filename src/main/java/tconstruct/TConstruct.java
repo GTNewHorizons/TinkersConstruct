aa8e3891718388c7bcee7e1912a0e12c71b2b8bb
AlexanderC <alexanderchenhall@protonmail.com>
2024-07-30 14:15:29 +1000

    Gradle Clean

--- src/main/java/tconstruct/TConstruct.java
+++ src/main/java/tconstruct/TConstruct.java
@@ -84,25 +84,25 @@ import tconstruct.world.village.VillageToolStationHandler;
  * @author mDiyo
  */
 @Mod(
-    modid = TConstruct.modID,
-    name = "TConstruct",
-    version = TConstruct.modVersion,
-    dependencies = "required-after:Forge@[10.13.3.1384,11.14);"
-        + "required-after:Mantle@[0.3.2,1.7.10),[1.7.10-0.3.2,);"
-        + // make sure we still have the 0.3.2 requirement, even without the 1.7.10 prefix
-        "after:MineFactoryReloaded@[1.7.10R2.8.0RC7,);"
-        + "after:ThermalExpansion@[1.7.10R4.0.0RC2,);"
-        + "after:ThermalFoundation@[1.7.10R1.0.0RC3,);"
-        + "after:armourersWorkshop@[1.7.10-0.28.0,);"
-        + "after:CoFHAPI|energy;"
-        + "after:CoFHCore;"
-        + "after:battlegear2;"
-        + "after:ZeldaItemAPI;"
-        + "after:DynamicSkillsAPI;"
-        + "after:NotEnoughItems;"
-        + "after:Waila;"
-        + "before:GalacticraftCore;"
-        + "before:UndergroundBiomes")
+        modid = TConstruct.modID,
+        name = "TConstruct",
+        version = TConstruct.modVersion,
+        dependencies = "required-after:Forge@[10.13.3.1384,11.14);"
+                + "required-after:Mantle@[0.3.2,1.7.10),[1.7.10-0.3.2,);"
+                + // make sure we still have the 0.3.2 requirement, even without the 1.7.10 prefix
+                "after:MineFactoryReloaded@[1.7.10R2.8.0RC7,);"
+                + "after:ThermalExpansion@[1.7.10R4.0.0RC2,);"
+                + "after:ThermalFoundation@[1.7.10R1.0.0RC3,);"
+                + "after:armourersWorkshop@[1.7.10-0.28.0,);"
+                + "after:CoFHAPI|energy;"
+                + "after:CoFHCore;"
+                + "after:battlegear2;"
+                + "after:ZeldaItemAPI;"
+                + "after:DynamicSkillsAPI;"
+                + "after:NotEnoughItems;"
+                + "after:Waila;"
+                + "before:GalacticraftCore;"
+                + "before:UndergroundBiomes")
 public class TConstruct {
 
     public static final String modVersion = Tags.VERSION;
@@ -130,8 +130,8 @@ public class TConstruct {
 
     /* Loads modules in a way that doesn't clutter the @Mod list */
     public static PulseManager pulsar = new PulseManager(
-        modID,
-        new ForgeCFG("TinkersModules", "Modules: Disabling these will disable a chunk of the mod"));
+            modID,
+            new ForgeCFG("TinkersModules", "Modules: Disabling these will disable a chunk of the mod"));
 
     public TConstruct() {
         if (Loader.isModLoaded("Natura")) {
--- src/main/java/tconstruct/items/tools/Excavator.java
+++ src/main/java/tconstruct/items/tools/Excavator.java
@@ -29,7 +29,7 @@ public class Excavator extends AOEHarvestTool {
     }
 
     static Material[] materials = { Material.grass, Material.ground, Material.sand, Material.snow, Material.craftedSnow,
-        Material.clay };
+            Material.clay };
 
     @Override
     public Item getHeadItem() {
