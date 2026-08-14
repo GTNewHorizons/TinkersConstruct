e5b4f47db81b3f65558fc2773fa6d8fb9398e0f9
Pxx500 <81298696+Pxx500@users.noreply.github.com>
2026-08-14 16:50:55 +0200

    Fix AOE tool hit face desync (#310)

--- src/main/java/tconstruct/library/tools/AOEHarvestTool.java
+++ src/main/java/tconstruct/library/tools/AOEHarvestTool.java
@@ -4,9 +4,9 @@ import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.util.MovingObjectPosition;
 
 import tconstruct.library.util.AoEExclusionList;
+import tconstruct.library.util.BlockSideHitListener;
 
 public abstract class AOEHarvestTool extends HarvestTool {
 
@@ -26,6 +26,8 @@ public abstract class AOEHarvestTool extends HarvestTool {
 
     @Override
     public boolean onBlockStartBreak(ItemStack stack, int x, int y, int z, EntityPlayer player) {
+        if (player.worldObj.isRemote) return super.onBlockStartBreak(stack, x, y, z, player);
+
         // only effective materials matter. We don't want to aoe when breaking dirt with a hammer.
         String toolName = "tool." + getAOEToolName().toLowerCase();
         Block block = player.worldObj.getBlock(x, y, z);
@@ -39,10 +41,7 @@ public abstract class AOEHarvestTool extends HarvestTool {
 
         if (player.isSneaking()) return super.onBlockStartBreak(stack, x, y, z, player);
 
-        MovingObjectPosition mop = AbilityHelper.raytraceFromEntity(player.worldObj, player, false, 4.5d);
-        if (mop == null) return super.onBlockStartBreak(stack, x, y, z, player);
-        int sideHit = mop.sideHit;
-        // int sideHit = Minecraft.getMinecraft().objectMouseOver.sideHit;
+        int sideHit = BlockSideHitListener.getSideHit(player);
 
         // we successfully destroyed a block. time to do AOE!
         int xRange = breakRadius;
