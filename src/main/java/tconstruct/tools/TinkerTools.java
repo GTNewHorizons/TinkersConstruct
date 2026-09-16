0b4bd58d30edfa3e3210d3b88b5a00099c65d21e
aintripin <vladislavst3ady+kubuntu@gmail.com>
2026-09-16 10:26:57 -0700

    Backport the Width++/Height++ expander modifiers from TiC2
    Two modifier items, one per axis, AOE tools only, one slot each. Both on
    a hammer gives a full 5x5, which is TiC3's rule rather than TiC2's
    corner-clipped area. Icons reused from SlimeKnights' 1.12 release (MIT).

--- src/main/java/tconstruct/tools/TinkerTools.java
+++ src/main/java/tconstruct/tools/TinkerTools.java
@@ -79,6 +79,7 @@ import tconstruct.modifiers.tools.ModBlaze;
 import tconstruct.modifiers.tools.ModButtertouch;
 import tconstruct.modifiers.tools.ModCreativeToolModifier;
 import tconstruct.modifiers.tools.ModDurability;
+import tconstruct.modifiers.tools.ModExpander;
 import tconstruct.modifiers.tools.ModExtraModifier;
 import tconstruct.modifiers.tools.ModFlux;
 import tconstruct.modifiers.tools.ModInteger;
@@ -773,6 +774,21 @@ public class TinkerTools {
         ItemStack reinforcement = new ItemStack(TinkerTools.materials, 1, 43);
         ModifyBuilder.registerModifier(new ModReinforced(new ItemStack[] { reinforcement }, 16, 1));
 
+        ItemStack expanderHorizontal = new ItemStack(TinkerTools.materials, 1, 44);
+        ItemStack expanderVertical = new ItemStack(TinkerTools.materials, 1, 45);
+        ModifyBuilder.registerModifier(
+                new ModExpander(
+                        new ItemStack[] { expanderHorizontal },
+                        "Width++",
+                        "\u00a7a",
+                        StatCollector.translateToLocal("modifier.tool.width")));
+        ModifyBuilder.registerModifier(
+                new ModExpander(
+                        new ItemStack[] { expanderVertical },
+                        "Height++",
+                        "\u00a7a",
+                        StatCollector.translateToLocal("modifier.tool.height")));
+
         TConstructRegistry.registerActiveToolMod(new TActiveOmniMod());
     }
 
@@ -918,6 +934,36 @@ public class TinkerTools {
     private void craftingTableRecipes() {
         String[] patBlock = { "###", "###", "###" };
         String[] patSurround = { "###", "#m#", "###" };
+
+        // Expander modifier items (Width++/Height++): the pistons point the way the tool expands
+        GameRegistry.addRecipe(
+                new ShapedOreRecipe(
+                        new ItemStack(TinkerTools.materials, 1, 44),
+                        "glg",
+                        "pep",
+                        "glg",
+                        'g',
+                        "gearGtSmallSteel",
+                        'l',
+                        "blockLapis",
+                        'p',
+                        Blocks.sticky_piston,
+                        'e',
+                        Items.ender_pearl));
+        GameRegistry.addRecipe(
+                new ShapedOreRecipe(
+                        new ItemStack(TinkerTools.materials, 1, 45),
+                        "gpg",
+                        "lel",
+                        "gpg",
+                        'g',
+                        "gearGtSmallSteel",
+                        'l',
+                        "blockLapis",
+                        'p',
+                        Blocks.sticky_piston,
+                        'e',
+                        Items.ender_pearl));
 
         Object[] toolForgeBlocks = { "blockIron", "blockGold", Blocks.diamond_block, Blocks.emerald_block,
                 "blockCobalt", "blockArdite", "blockManyullyn", "blockCopper", "blockBronze", "blockTin",
